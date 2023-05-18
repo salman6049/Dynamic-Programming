b5fb434abc7c1331c77cd6c86568a5266fbc79a9
Salman Sigari <sigari.salman@gmail.com>
2023-05-17 21:37:34 -0400

    Add files via upload

---
+++ Model_Performance_Visualization.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.model_selection import learning_curve
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+# Assuming clf is your trained classifier and X_test and y_test are your testing data
+# Confusion Matrix
+plot_confusion_matrix(clf, X_test, y_test)
+plt.show()
+
+# ROC Curve
+plot_roc_curve(clf, X_test, y_test)
+plt.show()
+
+# Precision-Recall curve
+plot_precision_recall_curve(clf, X_test, y_test)
+plt.show()
+
+# Learning curve
+plot_learning_curve(clf, "Learning curve", X_train, y_train, cv=5)
+plt.show()
+
